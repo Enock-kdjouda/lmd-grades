9006f73a947506014322fb155bfc66ca2d0f545c
enock <enockdjoudas@gmail.com>
2025-01-05 17:58:40 +0100

    Ajout des fichiers manquants du chef de projet

---
+++ app/Models/ElementConstitutif.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ElementConstitutif extends Model
+{
+    use HasFactory;
+
+    protected $table = 'elements_constitutifs';
+    protected $fillable = ['code', 'nom', 'coefficient', 'ue_id'];
+
+    public function uniteEnseignement()
+    {
+        return $this->belongsTo(UniteEnseignement::class, 'ue_id');
+    }
+ 
+
+}
